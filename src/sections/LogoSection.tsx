4e0b5defbbe97a98c24a3ee50422966c5369ee90
RahulSingh <chouhanrahul1999@gmail.com>
2026-01-16 02:56:13 +0530

    added feature card

--- src/sections/LogoSection.tsx
+++ src/sections/LogoSection.tsx
@@ -9,29 +9,28 @@ const LogoIcon = ({
 }) => {
   return (
     <div className="flex-none flex-center marquee-item">
-      <img src={icon.imgPath} alt={icon.imgPath} />
-    </div>
-  );
-};
-const LogoSection = () => {
-  return (
-    <div className="md:my-20 my-10 relative">
-      <div className="gradient-edge">
-        <div className="gradient-edge">
-          <div className="marquee h-52">
-            <div className="marquee-box md:gap-12 gap-5">
-              {logoIconsList.map((icon) => (
-                <LogoIcon key={icon.imgPath} icon={icon} />
-              ))}
-              {logoIconsList.map((icon) => (
-                <LogoIcon key={icon.imgPath} icon={icon} />
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
+      <img src={icon.imgPath} alt="company logo" />
     </div>
   );
 };
 
-export default LogoSection;
+const LogoShowcase = () => (
+  <div className="md:my-20 my-10 relative">
+    <div className="gradient-edge" />
+    <div className="gradient-edge" />
+
+    <div className="marquee h-52">
+      <div className="marquee-box md:gap-12 gap-5">
+        {logoIconsList.map((icon, index) => (
+          <LogoIcon key={index} icon={icon} />
+        ))}
+
+        {logoIconsList.map((icon, index) => (
+          <LogoIcon key={index} icon={icon} />
+        ))}
+      </div>
+    </div>
+  </div>
+);
+
+export default LogoShowcase;
