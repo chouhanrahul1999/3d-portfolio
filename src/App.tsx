e3fb26a496844c8d45a454c938abb9b89b628cb7
RahulSingh <chouhanrahul1999@gmail.com>
2026-01-17 02:04:06 +0530

    reated left section of experience section

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import ExperienceSection from "./sections/ExperienceSection";
 import FeatureCards from "./sections/FeatureCards ";
 import Hero from "./sections/Hero";
 import LogoSection from "./sections/LogoSection";
@@ -13,6 +14,7 @@ function App() {
       <ShowcaseSection />
       <LogoSection />
       <FeatureCards />
+      <ExperienceSection />
     </main>
   );
 }
